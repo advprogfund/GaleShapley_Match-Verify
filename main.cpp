b563235d6cf23056a39a966c303307ac5ae5bba5
advprogfund <gabrielfernandez@ufl.edu>
2026-01-30 19:11:13 -0500

    Setting up algorithm.

--- main.cpp
+++ main.cpp
@@ -5,3 +5,31 @@ int main()
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+// PSUEDOCODE BASED ON CHAPTER-1-2026.pptx SLIDES FROM CLASS FILES SECTION
+// Pass a student in and determine which hospital should take them based on the algorithm.
+// Return the student, making the necessary changes within the algorithm as the while-loop completes.
+Student AssignReject()
+{
+    // While a hospital is free and has not been matched/assigned to every applicant
+    while ()
+    {
+        // Choose the hospital h
+        // first = first applicant on h's list who is yet to be assigned
+
+
+        if () // If first is free
+        {
+            // Assign h and first
+
+        }
+        else if () // If first prefers h to their current assignment h'
+        {
+            // Assign first and h, and h' has a free slot
+        }
+        else
+        {
+            // First rejects h
+        }
+    }
+}
